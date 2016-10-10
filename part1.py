38a142d0265904e2398a70d95c73c4726595344e
Rupal Sanghavi <rsanghavi@smu.edu>
2016-10-10 12:35:12 -0500

    Creating indice adj list correctly now.

--- part1.py
+++ part1.py
@@ -3,6 +3,11 @@ import math
 from scipy import spatial
 import networkx as nx
 import matplotlib.pyplot as plt
+import sys
+
+file = sys.argv[1]
+f = open(file, 'w')
+
 nnodes = 100
 avg_deg = 5
 r = math.sqrt((avg_deg+1)/(nnodes*math.pi))
@@ -14,17 +19,27 @@ G = nx.Graph()
 G.add_nodes_from(range(nnodes))
 G.add_edges_from(list(pairs))
 pos = dict(zip(range(nnodes),positions))
-#nx.draw(G,pos)
-#plt.show()
-#print(pos)
+# nx.draw(G,pos)
+# plt.show()
 
 adj_list = {}
 #for key, value in pos.items():
 #    adj_list.setdefault(key, []) #create list for values
 #    adj_list[key].append(value)#
 #print(pairs)
+
 for i in pairs:
     adj_list.setdefault(i[0], []) #create list for values
-    adj_list[i[0]].append(i[1])#
-for key, val in adj_list.iteritems():
+    adj_list.setdefault(i[1], []) #create list for values
+    adj_list[i[0]].append(i[1])
+    adj_list[i[1]].append(i[0])
+count = 0
+for key, val in adj_list.items():
+    count += 1
     print key, val
+    for p in pos[key]:
+        f.write(str(p) + " ")
+    f.write('\n')
+        #f.write(p) #[0] + " " + pos[key][1] + " ")
+f.close()
+print(count)
