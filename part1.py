78fdb0288c36e58c3626ac5a20aaf95f6db5d147
Rupal Sanghavi <rsanghavi@smu.edu>
2016-10-14 08:04:16 -0500

    fixed radius calc

--- part1.py
+++ part1.py
@@ -5,12 +5,35 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import sys
 
+
+def writetofile(pairs):
+    adj_list = {}
+    for i in pairs:
+        adj_list.setdefault(i[0], []) #create list for values
+        adj_list.setdefault(i[1], []) #create list for values
+        adj_list[i[0]].append(i[1])
+        adj_list[i[1]].append(i[0])
+    count = 0
+    for key, val in adj_list.items():
+        count += 1
+        #print key, val
+        for p in pos[key]:
+            f.write(str(p) + " ")
+        for p in val:
+            for q in pos[p]:
+                f.write(str(q) + " ")
+        f.write('S' + '\n')
+    f.close()
+    print(count)
+
+
 file = sys.argv[1]
 f = open(file, 'w')
 
-nnodes = 100
-avg_deg = 5
-r = math.sqrt((avg_deg+1)/(nnodes*math.pi))
+nnodes = 1000
+avg_deg = 32
+r = math.sqrt((avg_deg)/(nnodes*math.pi))
+
 #r = 0.15
 positions =  np.random.rand(nnodes,2)
 kdtree = spatial.KDTree(positions)
@@ -19,30 +42,6 @@ G = nx.Graph()
 G.add_nodes_from(range(nnodes))
 G.add_edges_from(list(pairs))
 pos = dict(zip(range(nnodes),positions))
-# nx.draw(G,pos)
-# plt.show()
-
-adj_list = {}
-#for key, value in pos.items():
-#    adj_list.setdefault(key, []) #create list for values
-#    adj_list[key].append(value)#
-#print(pairs)
-
-for i in pairs:
-    adj_list.setdefault(i[0], []) #create list for values
-    adj_list.setdefault(i[1], []) #create list for values
-    adj_list[i[0]].append(i[1])
-    adj_list[i[1]].append(i[0])
-count = 0
-for key, val in adj_list.items():
-    count += 1
-    print key, val
-    for p in pos[key]:
-        f.write(str(p) + " ")
-    for p in val:
-        for q in pos[p]:
-            f.write(str(q) + " ")
-    f.write('S' + '\n')
-        #f.write(p) #[0] + " " + pos[key][1] + " ")
-f.close()
-print(count)
+nx.draw(G,pos)
+plt.show()
+writetofile(pairs)
