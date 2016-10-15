32294b29490c98e56357c7884a6880380c31dcda
Rupal Sanghavi <rsanghavi@smu.edu>
2016-10-14 22:23:55 -0500

    points distributed on disk

--- part1.py
+++ part1.py
@@ -4,6 +4,7 @@ from scipy import spatial
 import networkx as nx
 import matplotlib.pyplot as plt
 import sys
+import random
 
 
 def writetofile(pairs):
@@ -30,18 +31,33 @@ def writetofile(pairs):
 file = sys.argv[1]
 f = open(file, 'w')
 
-nnodes = 1000
-avg_deg = 32
+nnodes = 16000
+avg_deg = 64
 r = math.sqrt((avg_deg)/(nnodes*math.pi))
+print(r)
 
 #r = 0.15
-positions =  np.random.rand(nnodes,2)
+#positions =  np.random.rand(nnodes,2)
+positions = []
+count = 0
+while (count < nnodes):
+    coordinates = []
+    rad = random.uniform(0,1)
+    deg = random.uniform(0,(2*math.pi))   #generate random degree
+    #print(deg)
+    coordinates.append(math.sqrt(rad)*math.cos(deg))
+    coordinates.append(math.sqrt(rad)*math.sin(deg))
+    # coordinates.append(math.sqrt(r)*math.cos(math.radians(deg)))
+    # coordinates.append(math.sqrt(r)*math.sin(math.radians(deg)))
+    #print(coordinates)
+    positions.append(coordinates)
+    count += 1
 kdtree = spatial.KDTree(positions)
 pairs = kdtree.query_pairs(r)
 G = nx.Graph()
 G.add_nodes_from(range(nnodes))
 G.add_edges_from(list(pairs))
 pos = dict(zip(range(nnodes),positions))
-nx.draw(G,pos)
-plt.show()
+# nx.draw(G,pos)
+# plt.show()
 writetofile(pairs)
