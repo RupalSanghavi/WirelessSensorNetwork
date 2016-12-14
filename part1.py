db11e93ebd3aff49c21e48a6ef347a1d3adf1ce4
Rupal Sanghavi <rsanghavi@smu.edu>
2016-12-13 18:47:28 -0600

    displaying min and max deg

--- part1.py
+++ part1.py
@@ -11,6 +11,7 @@ from pprint import pprint
 import random
 from multiprocessing import Queue
 from collections import deque
+import time
 
 cache = {'#348ABD': (0.20392156862745098, 0.5411764705882353, 0.7411764705882353), '#6d904f': (0.42745098039215684, 0.5647058823529412, 0.30980392156862746), '#ccebc4': (0.8, 0.9215686274509803, 0.7686274509803922), 'purple': (0.5019607843137255, 0.0, 0.5019607843137255), '#eeeeee': (0.9333333333333333, 0.9333333333333333, 0.9333333333333333), '#30a2da': (0.18823529411764706, 0.6352941176470588, 0.8549019607843137), 'yellow': (1.0, 1.0, 0.0), 'b': (0.0, 0.0, 1.0), '#E5E5E5': (0.8980392156862745, 0.8980392156862745, 0.8980392156862745), 'white': (1.0, 1.0, 1.0), 'cyan': (0.0, 1.0, 1.0), '#92C6FF': (0.5725490196078431, 0.7764705882352941, 1.0), '#bc82bd': (0.7372549019607844, 0.5098039215686274, 0.7411764705882353), '#009E73': (0.0, 0.6196078431372549, 0.45098039215686275), '#EAEAF2': (0.9176470588235294, 0.9176470588235294, 0.9490196078431372), '#ffed6f': (1.0, 0.9294117647058824, 0.43529411764705883), '#03ED3A': (0.011764705882352941, 0.9294117647058824, 0.22745098039215686), '#FF9F9A': (1.0, 0.6235294117647059, 0.6039215686274509), '0.60': (0.6, 0.6, 0.6), '#003FFF': (0.0, 0.24705882352941178, 1.0), '#EEEEEE': (0.9333333333333333, 0.9333333333333333, 0.9333333333333333), '#8C0900': (0.5490196078431373, 0.03529411764705882, 0.0), '#555555': (0.3333333333333333, 0.3333333333333333, 0.3333333333333333), '#006374': (0.0, 0.38823529411764707, 0.4549019607843137), '#00D7FF': (0.0, 0.8431372549019608, 1.0), 'red': (1.0, 0.0, 0.0), '#7600A1': (0.4627450980392157, 0.0, 0.6313725490196078), '#feffb3': (0.996078431372549, 1.0, 0.7019607843137254), 'gray': (0.5019607843137255, 0.5019607843137255, 0.5019607843137255), '#8EBA42': (0.5568627450980392, 0.7294117647058823, 0.25882352941176473), '#77BEDB': (0.4666666666666667, 0.7450980392156863, 0.8588235294117647), '#00FFCC': (0.0, 1.0, 0.8), 'y': (0.75, 0.75, 0), 'w': (1.0, 1.0, 1.0), '#C4AD66': (0.7686274509803922, 0.6784313725490196, 0.4), '#B0E0E6': (0.6901960784313725, 0.8784313725490196, 0.9019607843137255), '#81b1d2': (0.5058823529411764, 0.6941176470588235, 0.8235294117647058), '#f0f0f0': (0.9411764705882353, 0.9411764705882353, 0.9411764705882353), 'r': (1.0, 0.0, 0.0), '#b3de69': (0.7019607843137254, 0.8705882352941177, 0.4117647058823529), '.8': (0.8, 0.8, 0.8), '#e5ae38': (0.8980392156862745, 0.6823529411764706, 0.2196078431372549), '0.40': (0.4, 0.4, 0.4), '0.00': (0.0, 0.0, 0.0), '#bcbcbc': (0.7372549019607844, 0.7372549019607844, 0.7372549019607844), '#FFC400': (1.0, 0.7686274509803922, 0.0), 'green': (0.0, 0.5019607843137255, 0.0), '#D65F5F': (0.8392156862745098, 0.37254901960784315, 0.37254901960784315), '#988ED5': (0.596078431372549, 0.5568627450980392, 0.8352941176470589), '#467821': (0.27450980392156865, 0.47058823529411764, 0.12941176470588237), '#afeeee': (0.6862745098039216, 0.9333333333333333, 0.9333333333333333), 'darkgoldenrod': (0.7215686274509804, 0.5254901960784314, 0.043137254901960784), 'black': (0.0, 0.0, 0.0), '#4878CF': (0.2823529411764706, 0.47058823529411764, 0.8117647058823529), '#8dd3c7': (0.5529411764705883, 0.8274509803921568, 0.7803921568627451), '#6ACC65': (0.41568627450980394, 0.8, 0.396078431372549), '#fc4f30': (0.9882352941176471, 0.30980392156862746, 0.18823529411764706), '#CCB974': (0.8, 0.7254901960784313, 0.4549019607843137), '#8A2BE2': (0.5411764705882353, 0.16862745098039217, 0.8862745098039215), '#55A868': (0.3333333333333333, 0.6588235294117647, 0.40784313725490196), 'k': (0.0, 0.0, 0.0), '#0072B2': (0.0, 0.4470588235294118, 0.6980392156862745), '0.50': (0.5, 0.5, 0.5), '#E8000B': (0.9098039215686274, 0.0, 0.043137254901960784), '#7A68A6': (0.47843137254901963, 0.40784313725490196, 0.6509803921568628), 'magenta': (1.0, 0.0, 1.0), '#B8860B': (0.7215686274509804, 0.5254901960784314, 0.043137254901960784), '#CC79A7': (0.8, 0.4745098039215686, 0.6549019607843137), '#8b8b8b': (0.5450980392156862, 0.5450980392156862, 0.5450980392156862), '#FFFEA3': (1.0, 0.996078431372549, 0.6392156862745098), 'firebrick': (0.6980392156862745, 0.13333333333333333, 0.13333333333333333), '#64B5CD': (0.39215686274509803, 0.7098039215686275, 0.803921568627451), '#E24A33': (0.8862745098039215, 0.2901960784313726, 0.2), '#FFB5B8': (1.0, 0.7098039215686275, 0.7215686274509804), '#F0E442': (0.9411764705882353, 0.8941176470588236, 0.25882352941176473), '0.75': (0.75, 0.75, 0.75), 'blue': (0.0, 0.0, 1.0), '#FBC15E': (0.984313725490196, 0.7568627450980392, 0.3686274509803922), 'c': (0.0, 0.75, 0.75), '#777777': (0.4666666666666667, 0.4666666666666667, 0.4666666666666667), '#8172B2': (0.5058823529411764, 0.4470588235294118, 0.6980392156862745), '#bfbbd9': (0.7490196078431373, 0.7333333333333333, 0.8509803921568627), '#cbcbcb': (0.796078431372549, 0.796078431372549, 0.796078431372549), '.15': (0.15, 0.15, 0.15), '#C44E52': (0.7686274509803922, 0.3058823529411765, 0.3215686274509804), '#56B4E9': (0.33725490196078434, 0.7058823529411765, 0.9137254901960784), '#97F0AA': (0.592156862745098, 0.9411764705882353, 0.6666666666666666), 'g': (0.0, 0.5, 0.0), '#A60628': (0.6509803921568628, 0.023529411764705882, 0.1568627450980392), '#fdb462': (0.9921568627450981, 0.7058823529411765, 0.3843137254901961), '0.70': (0.7, 0.7, 0.7), '#4C72B0': (0.2980392156862745, 0.4470588235294118, 0.6901960784313725), '0.5': (0.5, 0.5, 0.5), '#D0BBFF': (0.8156862745098039, 0.7333333333333333, 1.0), '#017517': (0.00392156862745098, 0.4588235294117647, 0.09019607843137255), '#001C7F': (0.0, 0.10980392156862745, 0.4980392156862745), '#fa8174': (0.9803921568627451, 0.5058823529411764, 0.4549019607843137), 'm': (0.75, 0, 0.75), '#B47CC7': (0.7058823529411765, 0.48627450980392156, 0.7803921568627451), '#D55E00': (0.8352941176470589, 0.3686274509803922, 0.0)}
 
@@ -41,7 +42,7 @@ def smallestLastt(nnodes, adj_list, degrees):
 
 
 
-def smallestLast(nnodes, adj_list, adj_list_copy, degrees, smallestFirst):
+def smallestLast(nnodes, adj_list, adj_list_copy, degrees, smallestFirst, minDegVert, maxDegVert):
     print("nothing")
     orderedDict = OrderedDict(sorted(adj_list.items(), key=lambda t: len(t[1])))
     sum = 0
@@ -57,6 +58,7 @@ def smallestLast(nnodes, adj_list, adj_list_copy, degrees, smallestFirst):
     minDeg = len(orderedDict.items()[0][1])
     print("MIN DEGREE: "+ str(minDeg))
     print("MAX DEGREE: "+ str(maxDeg))
+
     degreesToVertices = {}  #key: degree, value: dictionary of vertices with that degree
     for vert,neighbors in degrees.iteritems():
         #if that vertice is not already in that degree's dict
@@ -69,7 +71,11 @@ def smallestLast(nnodes, adj_list, adj_list_copy, degrees, smallestFirst):
     for degree in degreesToVertices:
         degreesList.append(degree)
         degreesToNumVertices.append(len(degreesToVertices[degree]))
-
+    for vert in degreesToVertices[maxDeg]:
+        maxDegVert.append(vert)
+    for vert in degreesToVertices[minDeg]:
+        minDegVert.append(vert)
+    #maxDegVert = degreesToVertices[maxDeg]
     degreesList, degreesToNumVertices = zip(*sorted(zip(degreesList,degreesToNumVertices)))
 
      #DEGREE DISTRIBUTION PLOT
@@ -336,7 +342,7 @@ def color(adj_list, smallestFirst, colorClassSizes, parallelColors, colorToVert,
     colorClassSizes = list(reversed(colorClassSizes))
     parallelColors = list(reversed(parallelColors))
     # print("length: " + str(len(colorClassSizes)))
-    print(colors)
+    print("Colors in RGG: " + str(colors))
 
     # plt.gcf().clear()
     # barlist = plt.bar(np.array(range(0,colors)),np.array(colorClassSizes))
@@ -356,20 +362,33 @@ def createBipartiteGraphs(G, pos, pairs, adj_list, smallestFirst, colorClassSize
     colorClassSizes = list(reversed(colorClassSizes))
     print("MAX COLOR CLASS SIZE 2: " + str(colorClassSizes[0]))
     parallelColors = list(reversed(parallelColors))
-    for i in range(0,2):
+    colorToVertWithDicts = {}
+    for color in colorToVert:
+        colorToVertWithDicts[color] = dict((k,True) for k in colorToVert[color])
+    mostEdges = 1
+    secondMostEdges = 0
+    mostOptimalColors = []
+    secondMostOptimalColors = []
+    bestBackboneVertex = 0
+    secondBestBackboneVertex = 0
+    bestBipartitePairs = set()
+    secondBestBipartitePairs = set()
+    bestSubgraphAdjList = {}
+    secondBestSubgraphAdjList = {}
+    for j in range(0,len(color1)):
         subgraphAdjList = {}
         bipartitePairs = set()
         for pair in pairs:
             tuple = ()
             keep = False
-            if pair[0] in colorToVert[parallelColors[color1[i]]]:
-                if pair[1] in colorToVert[parallelColors[color2[i]]]:
+            if pair[0] in colorToVertWithDicts[parallelColors[color1[j]]]:
+                if pair[1] in colorToVertWithDicts[parallelColors[color2[j]]]:
                     tuple = (pair[0], pair[1])
                     #bipartitePairs[pair[0]] = pair[1]
                     keep = True
                     bipartitePairs.add(tuple)
-            elif pair[1] in colorToVert[parallelColors[color1[i]]]:
-                 if pair[0] in colorToVert[parallelColors[color2[i]]]:
+            elif pair[1] in colorToVertWithDicts[parallelColors[color1[j]]]:
+                 if pair[0] in colorToVertWithDicts[parallelColors[color2[j]]]:
                      tuple = (pair[1], pair[0])
                      #bipartitePairs[pair[1]] = pair[0]
                      keep = True
@@ -407,6 +426,8 @@ def createBipartiteGraphs(G, pos, pairs, adj_list, smallestFirst, colorClassSize
                 maxEdges = edges
         #With best Vertex
         print("Edges in backbone: " + str(maxEdges))
+
+
         queue = deque()
         visited = {}
         startVertex = bestStartingVertex
@@ -429,7 +450,126 @@ def createBipartiteGraphs(G, pos, pairs, adj_list, smallestFirst, colorClassSize
         nx.draw_networkx_nodes(G,pos,nodelist=visited,node_color=visitedColors,node_size=20, alpha=0.8)
         nx.draw_networkx_edges(G,pos,edgelist=list(backbonePairs),width=0.5,alpha=0.5,edge_color='b')
         plt.show()
+        #see if it is one of the largest backbones
+        if(maxEdges > mostEdges):
+            mostOptimalColors.append(color1[j])
+            mostOptimalColors.append(color2[j])
+            bestBackboneVertex = bestStartingVertex
+            # bestBipartitePairs = backbonePairs
+            #bestSubgraphAdjList = subgraphAdjList
+        elif(maxEdges > secondMostEdges):
+            secondMostOptimalColors.append(color1[j])
+            secondMostOptimalColors.append(color2[j])
+            secondBestBackboneVertex = bestStartingVertex
+            # secondBestBipartitePairs = backbonePairs
+            #secondBestSubgraphAdjList = subgraphAdjList
 
+    # subgraphAdjList = {}
+    # bipartitePairs = set()
+    # for pair in pairs:
+    #     tuple = ()
+    #     keep = False
+    #     if pair[0] in colorToVertWithDicts[parallelColors[mostOptimalColors[0]]]:
+    #         if pair[1] in colorToVertWithDicts[parallelColors[mostOptimalColors[1]]]:
+    #             tuple = (pair[0], pair[1])
+    #             #bipartitePairs[pair[0]] = pair[1]
+    #             keep = True
+    #             bipartitePairs.add(tuple)
+    #     elif pair[1] in colorToVertWithDicts[parallelColors[mostOptimalColors[0]]]:
+    #          if pair[0] in colorToVertWithDicts[parallelColors[mostOptimalColors[1]]]:
+    #              tuple = (pair[1], pair[0])
+    #              #bipartitePairs[pair[1]] = pair[0]
+    #              keep = True
+    #              bipartitePairs.add(tuple)
+    # #create adj list for subgraph
+    # for i in bipartitePairs:
+    #     if i[0] not in subgraphAdjList:
+    #         subgraphAdjList[i[0]] = {}
+    #     if i[1] not in subgraphAdjList:
+    #         subgraphAdjList[i[1]] = {}
+    #     subgraphAdjList[i[0]][i[1]] = True
+    #     subgraphAdjList[i[1]][i[0]] = True
+    # #breadth first search
+    # print("HI")
+    # backbonePairs = set()
+    # maxEdges = 0
+    # for vertex in subgraphAdjList:
+    #     edges = 0
+    #     vertQueue = deque()
+    #     visited = {}
+    #     startVertex = vertex
+    #     visited[startVertex] = True
+    #     # currVertex = startVertex
+    #     vertQueue.append(startVertex)
+    #     while len(vertQueue) != 0:
+    #         currVertex = vertQueue.popleft()
+    #         for neighbor in subgraphAdjList[currVertex]:
+    #             if neighbor not in visited:
+    #                 visited[neighbor] = True
+    #                 vertQueue.append(neighbor)
+    #             edges += 1
+    #
+    # #With best Vertex
+    # print("Edges in FINAL: " + str(maxEdges))
+    #
+    #
+    # queue = deque()
+    # visited = {}
+    # startVertex = bestBackboneVertex
+    # visited[startVertex] = True
+    # # currVertex = startVertex
+    # queue.append(startVertex)
+    # while len(queue) != 0:
+    #     currVertex = queue.popleft()
+    #     for neighbor in subgraphAdjList[currVertex]:
+    #         if neighbor not in visited:
+    #             visited[neighbor] = True
+    #             queue.append(neighbor)
+    #             tuple = (currVertex,neighbor)
+    #             backbonePairs.add(tuple)
+    # #display largest component aka backbone
+    # visitedColors = []
+    # for vertex in visited:
+    #     visitedColors.append(vertToColor[vertex])
+    # plt.gcf().clear()
+    # nx.draw_networkx_nodes(G,pos,nodelist=visited,node_color=visitedColors,node_size=20, alpha=0.8)
+    # nx.draw_networkx_edges(G,pos,edgelist=list(backbonePairs),width=0.5,alpha=0.5,edge_color='b')
+    # plt.show()
+    #FIRST BACKBONE
+    # bestSubgraphAdjList = {}
+    # for i in bestBipartitePairs:
+    #     if i[0] not in bestSubgraphAdjList:
+    #         bestSubgraphAdjList[i[0]] = {}
+    #     if i[1] not in bestSubgraphAdjList:
+    #         bestSubgraphAdjList[i[1]] = {}
+    #     bestSubgraphAdjList[i[0]][i[1]] = True
+    #     bestSubgraphAdjList[i[1]][i[0]] = True
+    #
+    # queue = deque()
+    # visited = {}
+    # startVertex = bestBackboneVertex
+    # visited[startVertex] = True
+    # # currVertex = startVertex
+    # queue.append(startVertex)
+    # testEdges = 0
+    # while len(queue) != 0:
+    #     currVertex = queue.popleft()
+    #     for neighbor in bestSubgraphAdjList[currVertex]:
+    #         if neighbor not in visited:
+    #             visited[neighbor] = True
+    #             queue.append(neighbor)
+    #             tuple = (currVertex,neighbor)
+    #             bestBipartitePairs.add(tuple)
+    #         testEdges += 1
+    # #display largest component aka backbone
+    # visitedColors = []
+    # for vertex in visited:
+    #     visitedColors.append(vertToColor[vertex])
+    # plt.gcf().clear()
+    # nx.draw_networkx_nodes(G,pos,nodelist=visited,node_color=visitedColors,node_size=20, alpha=0.8)
+    # nx.draw_networkx_edges(G,pos,edgelist=list(bestBipartitePairs),width=0.5,alpha=0.5,edge_color='b')
+    # print("Test edges: " + str(testEdges))
+    # plt.show()
 
 
         #print(bipartitePairs)
@@ -505,13 +645,13 @@ file = "blah.txt"
 f = open(file, 'w')
 
 nnodes = 4000
-avg_deg = 40
+avg_deg = 32
 #r = math.sqrt(avg_deg/float(nnodes))  #for disk
 r = math.sqrt((avg_deg)/(nnodes*math.pi))  #for square
 
 #r = 0.15
 print(r)
-
+start = time.time()
 #r = 1
 positions = []
 positions =  np.random.rand(nnodes,2)
@@ -537,8 +677,8 @@ pairs = kdtree.query_pairs(r)
 #print(pairs)
 print("GRAPH EDGES: "+ str(len(pairs)) )
 G = nx.Graph()
-G.add_nodes_from(range(nnodes))
-G.add_edges_from(list(pairs))
+# G.add_nodes_from(range(nnodes))
+# G.add_edges_from(list(pairs))
 
 pos = dict(zip(range(nnodes),positions))
 # nx.draw(G,pos)
@@ -549,18 +689,34 @@ writetofile(adj_list, pairs)
 degrees = {}
 adj_list_copy = copy.deepcopy(adj_list)
 smallestFirst = []
-smallestLast(nnodes,adj_list, adj_list_copy,degrees, smallestFirst)
+minDeg = []
+maxDeg = []
+smallestLast(nnodes,adj_list, adj_list_copy,degrees, smallestFirst,minDeg, maxDeg)
 colorClassSizes = []
 parallelColors = []
 colorToVert = {}
 vertToColor = {}
 color(adj_list, smallestFirst, colorClassSizes, parallelColors, colorToVert, vertToColor)
+nx.draw_networkx_nodes(G,pos,nodelist=range(nnodes),node_color='r',node_size=20,alpha=0.8)
+print(maxDeg)
+nx.draw_networkx_nodes(G,pos,nodelist=maxDeg,node_color='yellow',node_size=100, alpha=0.8)
+nx.draw_networkx_nodes(G,pos,nodelist=minDeg,node_color='black',node_size=100, alpha=0.8)
+
+nx.draw_networkx_edges(G,pos,edgelist=list(pairs), width = 0.5, alpha =0.5, edge_color='b')
+
+#G.add_edges_from(list(pairs))
+#nx.draw(G,pos)
+plt.show()
+
+
 # plt.gcf().clear()
 # for color in colorToVert:
 #     nx.draw_networkx_nodes(G,pos,nodelist=colorToVert[color], node_color=color, node_size=20, alpha=0.8)
 # #G.add_edges_from(list(pairs))
 # nx.draw_networkx_edges(G,pos,edgelist=list(pairs), width = 0.5, alpha =0.5, edge_color='b')
 # plt.show()
-createBipartiteGraphs(G, pos, pairs, adj_list, smallestFirst, colorClassSizes, parallelColors, colorToVert, vertToColor)
 
+#createBipartiteGraphs(G, pos, pairs, adj_list, smallestFirst, colorClassSizes, parallelColors, colorToVert, vertToColor)
+end = time.time()
+print("Time Taken: " + str(end-start))
 #degreeCalc(nnodes,adj_list, degrees)
