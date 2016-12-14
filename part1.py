46af88ca8e89270921e4daae9425990ed6c83ce4
Rupal Sanghavi <rsanghavi@smu.edu>
2016-12-13 23:31:07 -0600

    displaying min and max edges, better visualization also

--- part1.py
+++ part1.py
@@ -12,6 +12,7 @@ import random
 from multiprocessing import Queue
 from collections import deque
 import time
+import string
 
 cache = {'#348ABD': (0.20392156862745098, 0.5411764705882353, 0.7411764705882353), '#6d904f': (0.42745098039215684, 0.5647058823529412, 0.30980392156862746), '#ccebc4': (0.8, 0.9215686274509803, 0.7686274509803922), 'purple': (0.5019607843137255, 0.0, 0.5019607843137255), '#eeeeee': (0.9333333333333333, 0.9333333333333333, 0.9333333333333333), '#30a2da': (0.18823529411764706, 0.6352941176470588, 0.8549019607843137), 'yellow': (1.0, 1.0, 0.0), 'b': (0.0, 0.0, 1.0), '#E5E5E5': (0.8980392156862745, 0.8980392156862745, 0.8980392156862745), 'white': (1.0, 1.0, 1.0), 'cyan': (0.0, 1.0, 1.0), '#92C6FF': (0.5725490196078431, 0.7764705882352941, 1.0), '#bc82bd': (0.7372549019607844, 0.5098039215686274, 0.7411764705882353), '#009E73': (0.0, 0.6196078431372549, 0.45098039215686275), '#EAEAF2': (0.9176470588235294, 0.9176470588235294, 0.9490196078431372), '#ffed6f': (1.0, 0.9294117647058824, 0.43529411764705883), '#03ED3A': (0.011764705882352941, 0.9294117647058824, 0.22745098039215686), '#FF9F9A': (1.0, 0.6235294117647059, 0.6039215686274509), '0.60': (0.6, 0.6, 0.6), '#003FFF': (0.0, 0.24705882352941178, 1.0), '#EEEEEE': (0.9333333333333333, 0.9333333333333333, 0.9333333333333333), '#8C0900': (0.5490196078431373, 0.03529411764705882, 0.0), '#555555': (0.3333333333333333, 0.3333333333333333, 0.3333333333333333), '#006374': (0.0, 0.38823529411764707, 0.4549019607843137), '#00D7FF': (0.0, 0.8431372549019608, 1.0), 'red': (1.0, 0.0, 0.0), '#7600A1': (0.4627450980392157, 0.0, 0.6313725490196078), '#feffb3': (0.996078431372549, 1.0, 0.7019607843137254), 'gray': (0.5019607843137255, 0.5019607843137255, 0.5019607843137255), '#8EBA42': (0.5568627450980392, 0.7294117647058823, 0.25882352941176473), '#77BEDB': (0.4666666666666667, 0.7450980392156863, 0.8588235294117647), '#00FFCC': (0.0, 1.0, 0.8), 'y': (0.75, 0.75, 0), 'w': (1.0, 1.0, 1.0), '#C4AD66': (0.7686274509803922, 0.6784313725490196, 0.4), '#B0E0E6': (0.6901960784313725, 0.8784313725490196, 0.9019607843137255), '#81b1d2': (0.5058823529411764, 0.6941176470588235, 0.8235294117647058), '#f0f0f0': (0.9411764705882353, 0.9411764705882353, 0.9411764705882353), 'r': (1.0, 0.0, 0.0), '#b3de69': (0.7019607843137254, 0.8705882352941177, 0.4117647058823529), '.8': (0.8, 0.8, 0.8), '#e5ae38': (0.8980392156862745, 0.6823529411764706, 0.2196078431372549), '0.40': (0.4, 0.4, 0.4), '0.00': (0.0, 0.0, 0.0), '#bcbcbc': (0.7372549019607844, 0.7372549019607844, 0.7372549019607844), '#FFC400': (1.0, 0.7686274509803922, 0.0), 'green': (0.0, 0.5019607843137255, 0.0), '#D65F5F': (0.8392156862745098, 0.37254901960784315, 0.37254901960784315), '#988ED5': (0.596078431372549, 0.5568627450980392, 0.8352941176470589), '#467821': (0.27450980392156865, 0.47058823529411764, 0.12941176470588237), '#afeeee': (0.6862745098039216, 0.9333333333333333, 0.9333333333333333), 'darkgoldenrod': (0.7215686274509804, 0.5254901960784314, 0.043137254901960784), 'black': (0.0, 0.0, 0.0), '#4878CF': (0.2823529411764706, 0.47058823529411764, 0.8117647058823529), '#8dd3c7': (0.5529411764705883, 0.8274509803921568, 0.7803921568627451), '#6ACC65': (0.41568627450980394, 0.8, 0.396078431372549), '#fc4f30': (0.9882352941176471, 0.30980392156862746, 0.18823529411764706), '#CCB974': (0.8, 0.7254901960784313, 0.4549019607843137), '#8A2BE2': (0.5411764705882353, 0.16862745098039217, 0.8862745098039215), '#55A868': (0.3333333333333333, 0.6588235294117647, 0.40784313725490196), 'k': (0.0, 0.0, 0.0), '#0072B2': (0.0, 0.4470588235294118, 0.6980392156862745), '0.50': (0.5, 0.5, 0.5), '#E8000B': (0.9098039215686274, 0.0, 0.043137254901960784), '#7A68A6': (0.47843137254901963, 0.40784313725490196, 0.6509803921568628), 'magenta': (1.0, 0.0, 1.0), '#B8860B': (0.7215686274509804, 0.5254901960784314, 0.043137254901960784), '#CC79A7': (0.8, 0.4745098039215686, 0.6549019607843137), '#8b8b8b': (0.5450980392156862, 0.5450980392156862, 0.5450980392156862), '#FFFEA3': (1.0, 0.996078431372549, 0.6392156862745098), 'firebrick': (0.6980392156862745, 0.13333333333333333, 0.13333333333333333), '#64B5CD': (0.39215686274509803, 0.7098039215686275, 0.803921568627451), '#E24A33': (0.8862745098039215, 0.2901960784313726, 0.2), '#FFB5B8': (1.0, 0.7098039215686275, 0.7215686274509804), '#F0E442': (0.9411764705882353, 0.8941176470588236, 0.25882352941176473), '0.75': (0.75, 0.75, 0.75), 'blue': (0.0, 0.0, 1.0), '#FBC15E': (0.984313725490196, 0.7568627450980392, 0.3686274509803922), 'c': (0.0, 0.75, 0.75), '#777777': (0.4666666666666667, 0.4666666666666667, 0.4666666666666667), '#8172B2': (0.5058823529411764, 0.4470588235294118, 0.6980392156862745), '#bfbbd9': (0.7490196078431373, 0.7333333333333333, 0.8509803921568627), '#cbcbcb': (0.796078431372549, 0.796078431372549, 0.796078431372549), '.15': (0.15, 0.15, 0.15), '#C44E52': (0.7686274509803922, 0.3058823529411765, 0.3215686274509804), '#56B4E9': (0.33725490196078434, 0.7058823529411765, 0.9137254901960784), '#97F0AA': (0.592156862745098, 0.9411764705882353, 0.6666666666666666), 'g': (0.0, 0.5, 0.0), '#A60628': (0.6509803921568628, 0.023529411764705882, 0.1568627450980392), '#fdb462': (0.9921568627450981, 0.7058823529411765, 0.3843137254901961), '0.70': (0.7, 0.7, 0.7), '#4C72B0': (0.2980392156862745, 0.4470588235294118, 0.6901960784313725), '0.5': (0.5, 0.5, 0.5), '#D0BBFF': (0.8156862745098039, 0.7333333333333333, 1.0), '#017517': (0.00392156862745098, 0.4588235294117647, 0.09019607843137255), '#001C7F': (0.0, 0.10980392156862745, 0.4980392156862745), '#fa8174': (0.9803921568627451, 0.5058823529411764, 0.4549019607843137), 'm': (0.75, 0, 0.75), '#B47CC7': (0.7058823529411765, 0.48627450980392156, 0.7803921568627451), '#D55E00': (0.8352941176470589, 0.3686274509803922, 0.0)}
 
@@ -79,11 +80,11 @@ def smallestLast(nnodes, adj_list, adj_list_copy, degrees, smallestFirst, minDeg
     degreesList, degreesToNumVertices = zip(*sorted(zip(degreesList,degreesToNumVertices)))
 
      #DEGREE DISTRIBUTION PLOT
-    # x = np.array(degreesList)
-    # y = np.array(degreesToNumVertices)
-    # plt.gcf().clear()
-    # barlist = plt.bar(x,y)
-    # plt.show()
+    x = np.array(degreesList)
+    y = np.array(degreesToNumVertices)
+    plt.gcf().clear()
+    barlist = plt.bar(x,y)
+    plt.show()
 
     #print(degrees)
     #pprint(degreesToVertices)
@@ -194,13 +195,14 @@ def smallestLast(nnodes, adj_list, adj_list_copy, degrees, smallestFirst, minDeg
     # print(maxDeg)
     revOrgDegList = list(reversed(originalDegrees))
     revDelDegList = list(reversed(deletedDegrees))
+    print("Max Degree when deleted: " + str(max(revDelDegList)))
     x1 = np.array(revOrgDegList)
     x2 = np.array(revDelDegList)
     y = np.array(range(0,nnodes))
-    # plt.gcf().clear()
-    # plt.plot(y, x1)
-    # plt.plot(y, x2)
-    # plt.show()
+    plt.gcf().clear()
+    plt.plot(y, x1)
+    plt.plot(y, x2)
+    plt.show()
 
 
 
@@ -346,11 +348,11 @@ def color(adj_list, smallestFirst, colorClassSizes, parallelColors, colorToVert,
     # print("length: " + str(len(colorClassSizes)))
     print("Colors in RGG: " + str(colors))
 
-    # plt.gcf().clear()
-    # barlist = plt.bar(np.array(range(0,colors)),np.array(colorClassSizes))
-    # for i in range(0,len(colorClassSizes)):
-    #     barlist[i].set_color(parallelColors[i])
-    # plt.show()
+    plt.gcf().clear()
+    barlist = plt.bar(np.array(range(0,colors)),np.array(colorClassSizes))
+    for i in range(0,len(colorClassSizes)):
+        barlist[i].set_color(parallelColors[i])
+    plt.show()
 
 def createBipartiteGraphs(G, pos, pairs, adj_list, smallestFirst, colorClassSizes, parallelColors, colorToVert, vertToColor):
 
@@ -461,6 +463,7 @@ def createBipartiteGraphs(G, pos, pairs, adj_list, smallestFirst, colorClassSize
         nx.draw_networkx_nodes(G,pos,nodelist=visited,node_color=visitedColors,node_size=20, alpha=0.8)
         nx.draw_networkx_edges(G,pos,edgelist=list(backbonePairs),width=0.5,alpha=0.5,edge_color='b')
         plt.show()
+
         #see if it is one of the largest backbones
         if(maxEdges > mostEdges):
             mostOptimalColors.append(color1[j])
@@ -655,12 +658,12 @@ def writetofile(adj_list,pairs):
 file = "blah.txt"
 f = open(file, 'w')
 
-nnodes = 4000
-avg_deg = 60
+nnodes = 1000
+avg_deg = 32
 #r = math.sqrt(avg_deg/float(nnodes))  #for disk
 r = math.sqrt((avg_deg)/(nnodes*math.pi))  #for square
 
-#r = 0.15
+#r = 0.3
 print(r)
 start = time.time()
 #r = 1
@@ -707,13 +710,52 @@ colorClassSizes = []
 parallelColors = []
 colorToVert = {}
 vertToColor = {}
+
+
+#maxPairs = [item for item in ]
+# pairs = dict(pairs)
+# for maxVert in maxDeg:
+#     if maxVert in pairs:
+#         maxPairs.add(pairs[maxVert])
+
+
 color(adj_list, smallestFirst, colorClassSizes, parallelColors, colorToVert, vertToColor)
-nx.draw_networkx_nodes(G,pos,nodelist=range(nnodes),node_color='r',node_size=20,alpha=0.8)
+
+nx.draw_networkx_nodes(G,pos,nodelist=range(nnodes),node_color='r',node_size=20,alpha=0.4)
 print(maxDeg)
 nx.draw_networkx_nodes(G,pos,nodelist=maxDeg,node_color='yellow',node_size=100, alpha=0.8)
 nx.draw_networkx_nodes(G,pos,nodelist=minDeg,node_color='black',node_size=100, alpha=0.8)
 
-nx.draw_networkx_edges(G,pos,edgelist=list(pairs), width = 0.5, alpha =0.5, edge_color='b')
+
+for vert in maxDeg:
+    maxPairs = set()
+    for neighbor in adj_list[vert]:
+        maxPairs.add((vert,neighbor))
+    nx.draw_networkx_edges(G,pos,edgelist=list(maxPairs), width = 0.9, alpha =0.8, edge_color='black')
+for vert in minDeg:
+    minPairs = set()
+    for neighbor in adj_list[vert]:
+        minPairs.add((vert,neighbor))
+    nx.draw_networkx_edges(G,pos,edgelist=list(minPairs), width = 0.9, alpha =0.8, edge_color='black')
+
+#nx.draw_networkx_edges(G,pos,edgelist=list(pairs), width = 0.5, alpha =0.5, edge_color='b')
+
+# for i in adj_list:
+#     print adj_list[i]
+
+#WALKTHROUGH
+# pprint(adj_list)
+# labels = {}
+# letter = 0
+# for i in range(0,16):
+#     labels[i] = i #string.ascii_lowercase[letter]
+#     #letter +=1
+# nx.draw_networkx_labels(G,pos,labels,font_size=10)
+# nx.draw_networkx_nodes(G,pos,nodelist=range(nnodes),node_color='r',node_size=150,alpha=0.8)
+
+
+
+
 
 #G.add_edges_from(list(pairs))
 #nx.draw(G,pos)
